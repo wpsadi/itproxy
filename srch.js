0acaabf3179e2f7456c41c62fad0f9592b94c36d
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-11-26 12:11:03 +0530

    Add files via upload

---
+++ srch.js
@@ -0,0 +1,449 @@
+const data = {
+    1: {
+        ApplicationNo: "131230000648",
+        EnrollmentNo: "00116401523",
+        User: "Aditya Goel"
+    },
+    2: {
+        ApplicationNo: "131230001956",
+        EnrollmentNo: "00216401523",
+        User: "Divyam Gupta"
+    },
+    3: {
+        ApplicationNo: "131230002479",
+        EnrollmentNo: "00316401523",
+        User: "Aayush"
+    },
+    4: {
+        ApplicationNo: "131230002605",
+        EnrollmentNo: "00416401523",
+        User: "Abhishek Tomar"
+    },
+    5: {
+        ApplicationNo: "131230002795",
+        EnrollmentNo: "00516401523",
+        User: "TUSHAR"
+    },
+    6: {
+        ApplicationNo: "131230003202",
+        EnrollmentNo: "00616401523",
+        User: "VANSH TIBREWAL"
+    },
+    7: {
+        ApplicationNo: "131230003303",
+        EnrollmentNo: "00716401523",
+        User: "Keshav Kumar"
+    },
+    8: {
+        ApplicationNo: "131230003568",
+        EnrollmentNo: "00816401523",
+        User: "Ansh Tomar"
+    },
+    9: {
+        ApplicationNo: "131230004009",
+        EnrollmentNo: "00916401523",
+        User: "Harsh Kumar Sharma"
+    },
+    10: {
+        ApplicationNo: "131230004654",
+        EnrollmentNo: "01016401523",
+        User: "RISHIKANT YADAV"
+    },
+    11: {
+        ApplicationNo: "131230004871",
+        EnrollmentNo: "01116401523",
+        User: "santosh rawat"
+    },
+    12: {
+        ApplicationNo: "131230005654",
+        EnrollmentNo: "01216401523",
+        User: "Chandrashekhar Singh"
+    },
+    13: {
+        ApplicationNo: "131230006053",
+        EnrollmentNo: "01316401523",
+        User: "Shreyansh Jain"
+    },
+    14: {
+        ApplicationNo: "131230006254",
+        EnrollmentNo: "01416401523",
+        User: "MANGLAM SINHA"
+    },
+    15: {
+        ApplicationNo: "131230008106",
+        EnrollmentNo: "01516401523",
+        User: "Himanshu Tomar"
+    },
+    16: {
+        ApplicationNo: "131230008188",
+        EnrollmentNo: "01616401523",
+        User: "Ishant"
+    },
+    17: {
+        ApplicationNo: "131230008316",
+        EnrollmentNo: "01716401523",
+        User: "AYUSH KUMAR"
+    },
+    18: {
+        ApplicationNo: "131230008543",
+        EnrollmentNo: "01816401523",
+        User: "JASPREET SINGH"
+    },
+    19: {
+        ApplicationNo: "131230009223",
+        EnrollmentNo: "01916401523",
+        User: "Harshit Rastogi"
+    },
+    20: {
+        ApplicationNo: "131230010193",
+        EnrollmentNo: "02016401523",
+        User: "Abhijeet Sinha"
+    },
+    21: {
+        ApplicationNo: "131230010522",
+        EnrollmentNo: "02116401523",
+        User: "KAIF KHAN"
+    },
+    22: {
+        ApplicationNo: "131230010854",
+        EnrollmentNo: "02216401523",
+        User: "Nikhil Rai"
+    },
+    23: {
+        ApplicationNo: "131230010972",
+        EnrollmentNo: "02316401523",
+        User: "PARTH ALAGH"
+    },
+    24: {
+        ApplicationNo: "131230011086",
+        EnrollmentNo: "02416401523",
+        User: "tejashvi gautam"
+    },
+    25: {
+        ApplicationNo: "131230011428",
+        EnrollmentNo: "02516401523",
+        User: "PARSHANT VERMA"
+    },
+    26: {
+        ApplicationNo: "131230013115",
+        EnrollmentNo: "02616401523",
+        User: "SAMARTH AGRAWAL"
+    },
+    27: {
+        ApplicationNo: "131230013769",
+        EnrollmentNo: "02716401523",
+        User: "Jagtesh Singh"
+    },
+    28: {
+        ApplicationNo: "131230013807",
+        EnrollmentNo: "02816401523",
+        User: "MOHD AMIL"
+    },
+    29: {
+        ApplicationNo: "131230014211",
+        EnrollmentNo: "02916401523",
+        User: "Archit Taneja"
+    },
+    30: {
+        ApplicationNo: "131230014978",
+        EnrollmentNo: "03016401523",
+        User: "Bhavya Yadav"
+    },
+    31: {
+        ApplicationNo: "131230016945",
+        EnrollmentNo: "03116401523",
+        User: "Mayank"
+    },
+    32: {
+        ApplicationNo: "131230017187",
+        EnrollmentNo: "03216401523",
+        User: "Abhinav Gupta"
+    },
+    33: {
+        ApplicationNo: "131230017462",
+        EnrollmentNo: "03316401523",
+        User: "Aditya Yadav"
+    },
+    34: {
+        ApplicationNo: "131230017742",
+        EnrollmentNo: "03416401523",
+        User: "MD AAYAN"
+    },
+    35: {
+        ApplicationNo: "131230018195",
+        EnrollmentNo: "03516401523",
+        User: "PUNEET KUMAR RITURAJ"
+    },
+    36: {
+        ApplicationNo: "131230018487",
+        EnrollmentNo: "03616401523",
+        User: "Kartik Kaparwan"
+    },
+    37: {
+        ApplicationNo: "131230019290",
+        EnrollmentNo: "03716401523",
+        User: "Naman Kohli"
+    },
+    38: {
+        ApplicationNo: "131230020142",
+        EnrollmentNo: "03816401523",
+        User: "AAYUSH KAUSHIK"
+    },
+    39: {
+        ApplicationNo: "131230021424",
+        EnrollmentNo: "03916401523",
+        User: "Harsh Pachauri"
+    },
+    40: {
+        ApplicationNo: "131230021751",
+        EnrollmentNo: "04016401523",
+        User: "Akshit Maheshwari"
+    },
+    41: {
+        ApplicationNo: "131230021915",
+        EnrollmentNo: "04116401523",
+        User: "SOMIL JAIN"
+    },
+    42: {
+        ApplicationNo: "131230021919",
+        EnrollmentNo: "04216401523",
+        User: "PRIYANSHU KALONDIA"
+    },
+    43: {
+        ApplicationNo: "131230022278",
+        EnrollmentNo: "04316401523",
+        User: "RAHUL KUMAR"
+    },
+    44: {
+        ApplicationNo: "131230023177",
+        EnrollmentNo: "04416401523",
+        User: "Prajal"
+    },
+    45: {
+        ApplicationNo: "131230023396",
+        EnrollmentNo: "04516401523",
+        User: "ANURAG LAKRA"
+    },
+    46: {
+        ApplicationNo: "131230023709",
+        EnrollmentNo: "04616401523",
+        User: "Kshitij"
+    },
+    47: {
+        ApplicationNo: "131230023812",
+        EnrollmentNo: "04716401523",
+        User: "Himanshu saini"
+    },
+    48: {
+        ApplicationNo: "131230025542",
+        EnrollmentNo: "04816401523",
+        User: "HARSH MISHRA"
+    },
+    49: {
+        ApplicationNo: "131230025717",
+        EnrollmentNo: "04916401523",
+        User: "Krish sharma"
+    },
+    50: {
+        ApplicationNo: "131230026051",
+        EnrollmentNo: "05016401523",
+        User: "Santosh Kumar Thakur"
+    },
+    51: {
+        ApplicationNo: "131230026696",
+        EnrollmentNo: "05116401523",
+        User: "Dushyant Kumar"
+    },
+    52: {
+        ApplicationNo: "131230027657",
+        EnrollmentNo: "05216401523",
+        User: "KRISHNA"
+    },
+    53: {
+        ApplicationNo: "131230028208",
+        EnrollmentNo: "05316401523",
+        User: "Navneet Aggarwal"
+    },
+    54: {
+        ApplicationNo: "131230028575",
+        EnrollmentNo: "05416401523",
+        User: "Dhruv Rathore"
+    },
+    55: {
+        ApplicationNo: "131230029460",
+        EnrollmentNo: "05516401523",
+        User: "Devashish Singh"
+    },
+    56: {
+        ApplicationNo: "131230030075",
+        EnrollmentNo: "05616401523",
+        User: "Gaurav"
+    },
+    57: {
+        ApplicationNo: "131230030597",
+        EnrollmentNo: "05716401523",
+        User: "Aritra shaw"
+    },
+    58: {
+        ApplicationNo: "131230031435",
+        EnrollmentNo: "05816401523",
+        User: "Harsh"
+    },
+    59: {
+        ApplicationNo: "131230031654",
+        EnrollmentNo: "05916401523",
+        User: "Aadesh kumar rai"
+    },
+    60: {
+        ApplicationNo: "131230032236",
+        EnrollmentNo: "06016401523",
+        User: "Kawaljeet Singh"
+    },
+    61: {
+        ApplicationNo: "131230032314",
+        EnrollmentNo: "06116401523",
+        User: "Sanchit Bhardwaj"
+    },
+    62: {
+        ApplicationNo: "131230032412",
+        EnrollmentNo: "06216401523",
+        User: "Abhishek Kumar Giri"
+    },
+    63: {
+        ApplicationNo: "131230032853",
+        EnrollmentNo: "06316401523",
+        User: "ANKIT"
+    },
+    64: {
+        ApplicationNo: "131230032854",
+        EnrollmentNo: "06416401523",
+        User: "Vishwakarma Rudrapratap"
+    },
+    65: {
+        ApplicationNo: "131230032884",
+        EnrollmentNo: "06516401523",
+        User: "VAIBHAV GUPTA"
+    },
+    66: {
+        ApplicationNo: "131230033187",
+        EnrollmentNo: "06616401523",
+        User: "Ansh Kumar Jha"
+    },
+    67: {
+        ApplicationNo: "131230033521",
+        EnrollmentNo: "06716401523",
+        User: "NIKHIL"
+    },
+    68: {
+        ApplicationNo: "131230034131",
+        EnrollmentNo: "06816401523",
+        User: "GAURAV"
+    },
+    69: {
+        ApplicationNo: "131230034806",
+        EnrollmentNo: "06916401523",
+        User: "Ankit Kumar Meena"
+    },
+    70: {
+        ApplicationNo: "131230034870",
+        EnrollmentNo: "07016401523",
+        User: "Vaibhav Mahandroo"
+    },
+    71: {
+        ApplicationNo: "131230036167",
+        EnrollmentNo: "07116401523",
+        User: "Prince Kumar Singh"
+    },
+    72: {
+        ApplicationNo: "131230036666",
+        EnrollmentNo: "07216401523",
+        User: "ABHINAV KUMAR JAISWAL"
+    },
+    73: {
+        ApplicationNo: "Foreign Student",
+        EnrollmentNo: "NA",
+        User: "Siddhath Kanodia"
+    },
+    74: {
+        ApplicationNo: "Foreign Student",
+        EnrollmentNo: "NA",
+        User: "Dyuksha Singhla"
+    },
+
+}
+
+const tbl = document.getElementsByTagName("table")[0]
+console.log(tbl)
+
+function trGen(...para){
+    let tr = document.createElement("tr")
+    tr.classList.add("f-data")
+    let count = 0
+    
+    for ( x of [...para]){
+        count++;
+        if (count == 2){
+            let td = document.createElement("td")
+            td.innerText = x;
+            td.classList.add("nm")
+            tr.append(td)
+            continue
+        }
+        if (count == 4){
+            let td = document.createElement("td")
+            td.innerText = x;
+            td.classList.add("enroll")
+            tr.append(td)
+            continue
+        }
+        let td = document.createElement("td")
+        td.innerText = x;
+        tr.append(td)
+    }
+    tbl.append(tr)
+}
+
+for (x in data){
+    trGen(x,data[x]["User"],data[x]["ApplicationNo"],data[x]["EnrollmentNo"])
+}
+// const div1 = document.createElement("div")
+// div1.style = "display:flex; flex-direction:row;"
+
+
+let iput = document.getElementById("field");
+
+// console.log(iput)
+
+iput.addEventListener("input",function(){
+    let val = (iput.value).trim();
+    let regx = new RegExp(val,"gi");
+
+    let row = document.getElementsByClassName("f-data")
+    for (x of row){
+        x.style.display = "table-row"
+
+    }
+
+    if ((val).length != 0){
+        // console.log(1)
+        let row = document.getElementsByClassName("f-data")
+
+        for (x of row){
+            let nm = (x.getElementsByClassName("nm")[0])
+            nm = nm.innerText;
+            // console.log(nm)
+            // console.log(nm)
+            let res = regx.test(nm)
+            if (res == false){
+                x.style.display = "none"
+                // console.log(res)
+            }
+        }
+
+        // console.log(row)
+
+        
+
+    }
+})
+
